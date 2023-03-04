e8399c09e6fd29a9f5fe0cf6a25d52e0dfaa9240
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-03-04 16:08:47 +0530

    commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const Playlist = new mongoose.model('Playlist', playlistSchema)
 
 // const createDocument = async () => {
 //     const abcd = new Playlist({
-            
+
 //                 name: 'fsdgwegg sir',
 //                 Dob: '11-march-1981',
 //                 occupation: 'software engineer',
@@ -35,20 +35,72 @@ const Playlist = new mongoose.model('Playlist', playlistSchema)
 //         role: 'TPO of company',
 //         membersince: 13})
 //     const fhfh=new Playlist({
-        
+
 //             name: 'dfgtgrg sir',
 //             Dob: '11-march-1981',
 //             occupation: 'software engineer',
 //             role: 'TPO of company',
 //             membersince: 13
-        
+
 //     })
 
 //     const ress = await Playlist.insertMany([abcd,abcd1,fhfh])
 //     console.log('data saved succesfully' + ress);
 // }
-const getDocument=async()=>{
-    const result=await Playlist.find();
+// const getDocument=async()=>{
+//     const result=await Playlist.find();
+//     console.log(result);
+// }
+// getDocument();
+
+// const getDocument1=async()=>{
+//     const result=await Playlist.find({membersince: 12}).select({name:1}).limit(3)
+//     console.log(result);
+// }
+// getDocument1();
+
+
+// const getDocument1=async()=>{
+//     const result=await Playlist.find().select({_id:0,name:1}).limit(2)
+//     console.log(result);
+// }
+// getDocument1();
+
+
+// const getDocument2 = async () => {
+//     const result = await Playlist.find({ membersince: 12 })
+//         .select({ _id: 0, name: 1, occupation: 1 });
+//     let mappedArray = result.map(item => item.name);
+//     const str = mappedArray.toString();
+//     var string = str.split(",");
+    
+//     console.log(string[0])
+//     console.log(string[1])
+
+// }
+// getDocument2();
+
+
+// const getDocument = async () => {
+//     const result = await Playlist.find({ membersince: 12 })
+//         .select({ _id: 0, name: 1, occupation: 1 });
+//     let mappedArray = result.map(item => item.name);
+//     const str = mappedArray.toString();
+//     var string = str.split(",");
+//     let mappedArra = result.map(item => item.occupation);
+//     const str1 = mappedArra.toString();
+//     var occ = str1.split(",");
+//     console.log(string[0]+' working as '+occ[0])
+//     console.log(string[1]+' working as '+occ[1])
+
+//     console.log(result);
+
+// }
+// getDocument();
+
+
+const getDocument1=async()=>{
+    const result=await Playlist.find({membersince:{$gte:16}}).select({_id:0,name:1})
     console.log(result);
 }
-getDocument();
+getDocument1();
